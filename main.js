ee49a313748fda4d99512a4b4e3d3056143ece50
Archil Dapkviashvili <archil.dapkviashvili@gmail.com>
2022-10-22 23:40:17 +0400

    add rgb button that selects random color on hover

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('.btn');
-const buttonClear = document.querySelector('.clear')
+const buttonClear = document.querySelector('.clear');
+const buttonRgb = document.querySelector('.rgb');
     
 function removeGrid(squares) {
     squares.forEach((square) => {
@@ -15,19 +16,18 @@ function makeGrid(gridNum = 16) {
         gridNum = 16;
     }
     for(let i = 0; i < (gridNum*gridNum+gridNum); i++) {
-        console.log(i);
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
         square.addEventListener("mouseover", () => {
-            square.style.cssText = `height: ${600/gridNum}px; width: ${600/gridNum}px; background: black;`;
+            square.style.background = "black";
         });
         square.style.cssText = `height: ${600/gridNum}px; width: ${600/gridNum}px;`
         if(i === 0 || i%(gridNum +1) === 0) {
             square.style.cssText = "width: 100%; border: 0; height: 0;";
         }
     }
-    squares = document.querySelectorAll('.square');    
+    squares = document.querySelectorAll('.square');
 }
 
 makeGrid();
@@ -45,3 +45,28 @@ buttonClear.addEventListener('click', () => {
     })
 });
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let buttonOn = 0;
+
+function makeRgb() {
+    if(buttonOn === 0) {
+        squares.forEach((square) => {
+            square.addEventListener('mouseover', () => {
+                square.style.background = `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)})`;
+            });
+        });
+        buttonOn++
+    } else if(buttonOn === 1) {
+        squares.forEach((square) => {
+            square.addEventListener('mouseover', () => {
+                square.style.background = `black`;
+            });
+        });
+        buttonOn--
+    }
+}
+
+buttonRgb.addEventListener('click', makeRgb);
